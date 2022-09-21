4e0e426eb82812babeb20c3ce951312d5d8438b4
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-21 15:32:12 -0500

    added comments

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const cartJson = require('./__mocks__/cart.json');
 const app = express();
 const PORT = 3001;
 
+// Cart API
 app.get('/cart', (req, res) => {
   res.send(cartJson);
 });
 
+// Handle default routes
 app.get('*', (req, res) => {
   res.status(404).send('resource not found');
 });
