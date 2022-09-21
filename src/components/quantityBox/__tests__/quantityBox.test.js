5a15cd11a5db12a9f25321c346157d7192c29dd7
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-21 17:09:04 -0500

    quantity test cases

--- src/components/quantityBox/__tests__/quantityBox.test.js
+++ src/components/quantityBox/__tests__/quantityBox.test.js
@@ -1,8 +1,19 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import QuantityBox from '../index';
 
+const MIN_QUANTITY = 1;
+const MAX_QUANTITY = 10;
+
 test('renders QuantityBox with default props', () => {
-  
+    const { getByTestId } = render(<QuantityBox />);
+
+    const quantity =  getByTestId('quantity-box').innerHTML;
+    const decreaseButton =  getByTestId('quantity-decrease');
+    const increaseButton =  getByTestId('quantity-increase');
+
+    expect(quantity).toEqual('1');
+    expect(decreaseButton.disabled).toEqual(true);
+    expect(increaseButton.disabled).toEqual(false);
 });
 
 test('increase quantity by 1', () => {
