bebd505d1b9622ee0b9e00da36d396a02583c612
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-21 17:38:36 -0500

    quantity box

--- src/components/quantityBox/__tests__/quantityBox.test.js
+++ src/components/quantityBox/__tests__/quantityBox.test.js
@@ -1,8 +1,5 @@
 import { render, fireEvent } from '@testing-library/react';
-import QuantityBox from '../index';
-
-const MIN_QUANTITY = 1;
-const MAX_QUANTITY = 10;
+import QuantityBox, {MIN_QUANTITY, MAX_QUANTITY } from '../index';
 
 test('renders QuantityBox with default props', () => {
     const { getByTestId } = render(<QuantityBox />);
@@ -22,40 +19,40 @@ test('increase/decrease quantity by 1', () => {
     const decreaseButton =  getByTestId('quantity-decrease');
     const increaseButton =  getByTestId('quantity-increase');
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual(1);
+    expect(getByTestId('quantity-box').innerHTML).toEqual('1');
 
     fireEvent.click(increaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual(2);
+    expect(getByTestId('quantity-box').innerHTML).toEqual('2');
 
     fireEvent.click(decreaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual(1);
+    expect(getByTestId('quantity-box').innerHTML).toEqual('1');
 });
 
 test('should not exceed max quantity', () => {
-    const { getByTestId } = render(<QuantityBox  initialQuantity={10} />);
+    const { getByTestId } = render(<QuantityBox  defaultQty={10} />);
     const increaseButton =  getByTestId('quantity-increase');
  
     fireEvent.click(increaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual(MAX_QUANTITY);
+    expect(getByTestId('quantity-box').innerHTML).toEqual(MAX_QUANTITY.toString());
     expect(increaseButton.disabled).toEqual(true);
 });
   
 test('should not exceed min quantity', () => {
-    const { getByTestId } = render(<QuantityBox  initialQuantity={1} />);
+    const { getByTestId } = render(<QuantityBox  defaultQty={1} />);
     const decreaseButton =  getByTestId('quantity-decrease');
 
     fireEvent.click(decreaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual(MIN_QUANTITY);
+    expect(getByTestId('quantity-box').innerHTML).toEqual(MIN_QUANTITY.toString());
     expect(decreaseButton.disabled).toEqual(true);
 });
 
 test('trigger a callback when quanity updated', () => {
     const callBack = jest.fn();
-    const { getByTestId } = render(<QuantityBox  initialQuantity={5} />);
+    const { getByTestId } = render(<QuantityBox  defaultQty={5} onChange={callBack} />);
     const increaseButton =  getByTestId('quantity-increase');
 
     fireEvent.click(increaseButton);
--- src/components/quantityBox/index.js
+++ src/components/quantityBox/index.js
@@ -1,7 +1,42 @@
-const QuantityBox = () =>{
+import {useState, useEffect} from 'react';
+
+export const MIN_QUANTITY = 1;
+export const MAX_QUANTITY = 10;
+
+/**
+ * Component QuantityBox
+ * @returns 
+ */
+const QuantityBox = (props) => {
+    const {defaultQty, onChange, minQuanity, maxQuantity} = props;
+    const [quanity, setQuantity] = useState(defaultQty);
+
+    const updateQuantity = (qty) => {
+        setQuantity(qty);
+        onChange?.(qty);
+    };
+
+    const increaseQuantity = () => updateQuantity(quanity+1);
+    const decreaseQuantity = () => updateQuantity(quanity-1);
+
+    useEffect(() => {
+        setQuantity(defaultQty);
+    }, [defaultQty]);
+
     return (
-        <div>QuantityBox</div>
+        <div>
+            <button data-testid="quantity-decrease" disabled={quanity <= minQuanity} onClick={decreaseQuantity}>-</button>
+                <div data-testid="quantity-box">{quanity}</div>
+            <button data-testid="quantity-increase" disabled={quanity >= maxQuantity} onClick={increaseQuantity}>+</button>
+        </div>
     )
+}
+
+QuantityBox.defaultProps = {
+    minQuanity: MIN_QUANTITY,
+    maxQuantity: MAX_QUANTITY,
+    onChange: null,
+    defaultQty: 1
 }
 
 export default QuantityBox;
