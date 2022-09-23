74e79b62d02a91409b4b6075647c3e29569455d1
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-22 23:58:01 -0500

    wishlist component

---
+++ src/modules/cart/wishList/index.js
@@ -0,0 +1,18 @@
+
+import LineItem from '../../../components/lineItem';
+
+/**
+ * Wish List
+ * @returns 
+ */
+const WishList = () =>{
+
+    return (
+        <>
+            <h2>Saved For Later Items (2)</h2>
+            <LineItem />
+        </>
+    )
+}
+
+export default WishList;
