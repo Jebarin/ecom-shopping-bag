a377238471190abadce3c0725a41aa219af9ab80
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-23 11:20:06 -0500

    custom store

---
+++ src/modules/cart/cartContext.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useMemo, useReducer } from 'react';
+import cartReducer from './store/reducer';
+import storeActions from './store/actions'
+import { getBagCount } from './cart.helper';
+
+const CartContext = createContext({});
+const initialState = {
+  bagCount: 0,
+  isCartLoaded: false
+};
+ 
+export const CartProvider = ({ value, children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  const reducerProps = useMemo(() => ({ ...state, dispatch }), [state]);
+
+  return (
+    <CartContext.Provider value={{ ...reducerProps, ...value }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+ 
+export const useCartContext = () => {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error('useCartContext must be used inside a CartProvider');
+  }
+
+  const { dispatch, ...state } = context;
+ 
+  return { 
+    ...state, 
+    bagCount: getBagCount(state.cartItems), 
+    ...storeActions(dispatch, state) 
+  };
+};
