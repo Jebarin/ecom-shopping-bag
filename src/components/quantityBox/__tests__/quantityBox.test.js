a1583f03c70239c17b54ff1388221715f47d6e6b
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-23 14:33:42 -0500

    cleanup

--- src/components/quantityBox/__tests__/quantityBox.test.js
+++ src/components/quantityBox/__tests__/quantityBox.test.js
@@ -1,12 +1,12 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import QuantityBox, {MIN_QUANTITY, MAX_QUANTITY } from '../index';
 
 test('renders QuantityBox with default props', () => {
-    const { getByTestId } = render(<QuantityBox />);
+    render(<QuantityBox />);
 
-    const quantity =  getByTestId('quantity-box').innerHTML;
-    const decreaseButton =  getByTestId('quantity-decrease');
-    const increaseButton =  getByTestId('quantity-increase');
+    const quantity =  screen.getByTestId('quantity-box').innerHTML;
+    const decreaseButton =  screen.getByTestId('quantity-decrease');
+    const increaseButton =  screen.getByTestId('quantity-increase');
 
     expect(quantity).toEqual('1');
     expect(decreaseButton.disabled).toEqual(true);
@@ -14,46 +14,46 @@ test('renders QuantityBox with default props', () => {
 });
 
 test('increase/decrease quantity by 1', () => {
-    const { getByTestId } = render(<QuantityBox />);
+    render(<QuantityBox />);
 
-    const decreaseButton =  getByTestId('quantity-decrease');
-    const increaseButton =  getByTestId('quantity-increase');
+    const decreaseButton =  screen.getByTestId('quantity-decrease');
+    const increaseButton =  screen.getByTestId('quantity-increase');
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual('1');
+    expect(screen.getByTestId('quantity-box').innerHTML).toEqual('1');
 
     fireEvent.click(increaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual('2');
+    expect(screen.getByTestId('quantity-box').innerHTML).toEqual('2');
 
     fireEvent.click(decreaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual('1');
+    expect(screen.getByTestId('quantity-box').innerHTML).toEqual('1');
 });
 
 test('should not exceed max quantity', () => {
-    const { getByTestId } = render(<QuantityBox  defaultQty={10} />);
-    const increaseButton =  getByTestId('quantity-increase');
+    render(<QuantityBox  defaultQty={10} />);
+    const increaseButton =  screen.getByTestId('quantity-increase');
  
     fireEvent.click(increaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual(MAX_QUANTITY.toString());
+    expect(screen.getByTestId('quantity-box').innerHTML).toEqual(MAX_QUANTITY.toString());
     expect(increaseButton.disabled).toEqual(true);
 });
   
 test('should not exceed min quantity', () => {
-    const { getByTestId } = render(<QuantityBox  defaultQty={1} />);
-    const decreaseButton =  getByTestId('quantity-decrease');
+    render(<QuantityBox  defaultQty={1} />);
+    const decreaseButton =  screen.getByTestId('quantity-decrease');
 
     fireEvent.click(decreaseButton);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual(MIN_QUANTITY.toString());
+    expect(screen.getByTestId('quantity-box').innerHTML).toEqual(MIN_QUANTITY.toString());
     expect(decreaseButton.disabled).toEqual(true);
 });
 
 test('trigger a callback when quanity updated', () => {
     const callBack = jest.fn();
-    const { getByTestId } = render(<QuantityBox  defaultQty={5} onChange={callBack} />);
-    const increaseButton =  getByTestId('quantity-increase');
+    render(<QuantityBox  defaultQty={5} onChange={callBack} />);
+    const increaseButton =  screen.getByTestId('quantity-increase');
 
     fireEvent.click(increaseButton);
     expect(callBack).toHaveBeenCalledTimes(1);
@@ -61,8 +61,8 @@ test('trigger a callback when quanity updated', () => {
 });
 
 test('readonly mode', () => {
-    const { getByTestId, queryByTestId } = render(<QuantityBox  qtyReadOnly={true} defaultQty={2} />);
+    render(<QuantityBox  qtyReadOnly={true} defaultQty={2} />);
 
-    expect(getByTestId('quantity-box').innerHTML).toEqual('2');
-    expect(queryByTestId(/quantity-increase/i)).toBeFalsy();
+    expect(screen.getByTestId('quantity-box').innerHTML).toEqual('2');
+    expect(screen.queryByTestId(/quantity-increase/i)).toBeFalsy();
 });
