ad7878823c9cc8e46d9526acb1a0cd3a6969aa17
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-22 23:30:43 -0500

    cart items component

---
+++ src/modules/cart/cartItems/cartItems.js
@@ -0,0 +1,18 @@
+
+import LineItem from '../../../components/lineItem';
+
+/**
+ * Cart Items
+ * @returns 
+ */
+const CartItems = () =>{
+
+    return (
+        <>
+            <h1>Active Items (1)</h1>
+            <LineItem />
+        </>
+    )
+}
+
+export default CartItems;
