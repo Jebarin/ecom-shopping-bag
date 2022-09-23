64c6bbfa1942712a99d7b4d74e7b7683d40c1d27
Jebarin <jebarin.johnrose@nutechnologyinc.com>
2022-09-23 13:00:50 -0500

    added wishlist

--- src/modules/cart/wishList/index.js
+++ src/modules/cart/wishList/index.js
@@ -1,16 +1,23 @@
 
 import LineItem from '../../../components/lineItem';
+import { useCartContext } from '../cartContext';
 
 /**
  * Wish List
  * @returns 
  */
 const WishList = () =>{
+    const {savedList, isCartLoaded } = useCartContext(); 
+
+    if(!isCartLoaded || savedList?.length ===0) return null;
 
     return (
         <>
-            <h2>Saved For Later Items (2)</h2>
-            <LineItem />
+            <h2>Saved For Later Items ({savedList.length})</h2>
+            {savedList.map((item)=>(
+                <LineItem  key={item.itemId} {...item}  qtyReadOnly={true} />
+            ))}
+            
         </>
     )
 }
